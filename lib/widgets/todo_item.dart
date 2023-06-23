21065a904e5212beb97da0c123b5c886b7f1104d
scamlux <scamlux@gmail.com>
2023-06-23 10:11:34 +0500

    first turn

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
+import '../model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem(
+      {Key? key,
+      required this.todo,
+      required this.onToDoChanged,
+      required this.onDeleteItem})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const  EdgeInsets.symmetric(vertical: 2),
+      child: ListTile(
+          onTap: () {
+            onToDoChanged(todo);
+          },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          tileColor: Colors.white,
+          leading:  Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue),
+          title: Text(
+            style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: todo.isDone
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none,
+            ),
+            todo.todoText!,
+          ),
+          trailing: Container(
+            padding: const EdgeInsets.all(0),
+            margin: const EdgeInsets.symmetric(vertical: 12),
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(
+              color: tdRed,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: IconButton(
+              color: Colors.white,
+              iconSize: 18,
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                onDeleteItem(todo.id);
+              },
+            ),
+          )),
+    );
+  }
+}
